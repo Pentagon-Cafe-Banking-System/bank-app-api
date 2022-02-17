3850aff9f28f5de8684f94fa891d04a8b877027d
Ktosi33 <54289137+Ktosi33@users.noreply.github.com>
2022-02-17 17:35:59 +0100

    refactor: replace finally block with private method

--- BankApp/Middleware/ErrorHandlerMiddleware.cs
+++ BankApp/Middleware/ErrorHandlerMiddleware.cs
@@ -7,41 +7,38 @@ public class ErrorHandlerMiddleware : IMiddleware
 {
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
-        var messageError = string.Empty;
-        var success = false;
-
         try
         {
             await next.Invoke(context);
-            success = true;
         }
         catch (BadRequestException badRequestException)
         {
             context.Response.StatusCode = 400;
-            messageError = badRequestException.Message;
+           
+            await WriteAsyncJson(context, badRequestException.Message);
         }
         catch (NotFoundException notFoundException)
         {
             context.Response.StatusCode = 404;
-            messageError = notFoundException.Message;
+            
+            await WriteAsyncJson(context, notFoundException.Message);
         }
         catch // (Exception e)
         {
-            context.Response.StatusCode = 500;
-            messageError = "Something went wrong";
+            context.Response.StatusCode = 500; 
+            await WriteAsyncJson(context, "Something went wrong");
         }
-        finally
-        {
-            if (!success)
-            {
-                context.Response.ContentType = "application/json";
-                await context.Response.WriteAsync(
-                    JsonSerializer.Serialize(
-                        new
-                        {
-                            message = messageError
-                        }));
-            }
-        }
+       
+    }
+
+    private async Task WriteAsyncJson(HttpContext context, string messageError)
+    {
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsync(
+            JsonSerializer.Serialize(
+                new
+                {
+                    message = messageError
+                }));
     }
 }
