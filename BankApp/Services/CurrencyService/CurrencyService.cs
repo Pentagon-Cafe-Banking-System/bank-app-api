260167feff7ddfa87eb73cff65e2c03884e57a2b
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-03-12 19:28:14 +0100

    feat: Add endpoint for currency list (#16)

---
+++ BankApp/Services/CurrencyService/CurrencyService.cs
@@ -0,0 +1,20 @@
+﻿using BankApp.Data;
+using BankApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankApp.Services.CurrencyService;
+
+public class CurrencyService : ICurrencyService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public CurrencyService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<Currency>> GetAllCurrenciesAsync()
+    {
+        return await _dbContext.Currencies.ToListAsync();
+    }
+}
