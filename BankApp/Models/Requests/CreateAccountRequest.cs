a9d2f3aed0d5a117f1e217f809691db04a915159
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-03-31 00:04:38 +0200

    fix: customer validation in create account validator

--- BankApp/Models/Requests/CreateAccountRequest.cs
+++ BankApp/Models/Requests/CreateAccountRequest.cs
@@ -1,7 +1,4 @@
-using System.ComponentModel.DataAnnotations.Schema;
 using BankApp.Data;
-using BankApp.Entities;
-using BankApp.Services.AccountService;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,24 +20,24 @@ public class CreateAccountRequestValidator : AbstractValidator<CreateAccountRequ
     public CreateAccountRequestValidator(ApplicationDbContext applicationDbContext)
     {
         RuleFor(e => e.Number).MustAsync(async (numberAccount, _) =>
-        {
-            var result = await applicationDbContext.Accounts.AnyAsync(number =>
-                number.Number == numberAccount
-            );
-            return !result;
-        }
-            ).WithMessage("Number already exists");
+            {
+                var result = await applicationDbContext.Accounts.AnyAsync(number =>
+                    number.Number == numberAccount
+                );
+                return !result;
+            }
+        ).WithMessage("Number already exists");
         RuleFor(e => e.Balance).GreaterThanOrEqualTo(0);
         RuleFor(e => e.TransferLimit).GreaterThan(0);
         RuleFor(e => e.IsActive).NotNull();
-        RuleFor(e => e.AccountTypeId).GreaterThanOrEqualTo((short)1).LessThanOrEqualTo((short)3);
-        RuleFor(e => e.CurrencyId).GreaterThanOrEqualTo((short)1).LessThanOrEqualTo((short)14);
-        RuleFor(e => e.CustomerId).MustAsync(async (customer, _) =>
+        RuleFor(e => e.AccountTypeId).GreaterThanOrEqualTo((short) 1).LessThanOrEqualTo((short) 3);
+        RuleFor(e => e.CurrencyId).GreaterThanOrEqualTo((short) 1).LessThanOrEqualTo((short) 14);
+        RuleFor(e => e.CustomerId).MustAsync(async (customerId, _) =>
             {
-                var result = await applicationDbContext.Customers.AnyAsync(customerId =>
-                    customerId.Id != customer
+                var result = await applicationDbContext.Customers.AnyAsync(customer =>
+                    customer.Id == customerId
                 );
-                return !result;
+                return result;
             }
         ).WithMessage("Customer doesn't exist");
     }
