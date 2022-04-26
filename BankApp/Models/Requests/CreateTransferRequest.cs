c88cb19904fb2fd506e2f1bc2992f4200683bb59
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-04-26 11:47:21 +0200

    fix: validation in create transfer request

--- BankApp/Models/Requests/CreateTransferRequest.cs
+++ BankApp/Models/Requests/CreateTransferRequest.cs
@@ -1,5 +1,4 @@
 using BankApp.Data;
-using BankApp.Exceptions;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,29 +15,67 @@ public class CreateTransferRequest
 
 public class CreateTransferRequestValidator : AbstractValidator<CreateTransferRequest>
 {
-    public CreateTransferRequestValidator(ApplicationDbContext applicationDbContext)
+    public CreateTransferRequestValidator(ApplicationDbContext dbContext)
     {
-        RuleFor(e => new {e.Amount, AccountId = e.SenderAccountId}).MustAsync(async (args, _) =>
+        RuleFor(e => new {e.Amount, e.SenderAccountId})
+            .MustAsync(async (args, cancellationToken) =>
+                {
+                    var senderAccount = await dbContext.Accounts
+                        .FindAsync(new object?[] {args.SenderAccountId}, cancellationToken: cancellationToken);
+                    return senderAccount != null;
+                }
+            )
+            .WithName("SenderAccountId")
+            .WithMessage("Sender account could not be found")
+            .MustAsync(async (args, cancellationToken) =>
+                {
+                    var senderAccount = await dbContext.Accounts
+                        .FindAsync(new object?[] {args.SenderAccountId}, cancellationToken: cancellationToken);
+                    return senderAccount?.Balance >= args.Amount;
+                }
+            )
+            .WithName("Amount")
+            .WithMessage("Insufficient funds")
+            .MustAsync(async (args, cancellationToken) =>
+                {
+                    var senderAccount = await dbContext.Accounts
+                        .FindAsync(new object?[] {args.SenderAccountId}, cancellationToken: cancellationToken);
+                    return args.Amount <= senderAccount?.TransferLimit;
+                }
+            )
+            .WithName("Amount")
+            .WithMessage("Transfer limit exceeded");
+
+        RuleFor(e => new {e.ReceiverAccountNumber, e.SenderAccountId})
+            .MustAsync(async (args, cancellationToken) =>
             {
-                var account = await applicationDbContext.Accounts.FindAsync(args.AccountId);
-                if (account == null)
-                    throw new AppException("Account with requested id could not be found");
-                var balance = account.Balance;
-                var transferLimit = account.TransferLimit;
-                var result = args.Amount <= balance && args.Amount <= transferLimit;
-                return result;
-            }
-        ).WithMessage("Amount is greater than balance or transfer limit");
-        RuleFor(e => e.Amount).GreaterThan(0);
-        RuleFor(e => e.ReceiverAccountNumber).MustAsync(async (numberAccount, _) =>
-            {
-                var result = await applicationDbContext.Accounts.AnyAsync(number =>
-                    number.Number == numberAccount
-                );
-                return result;
-            }
-        ).WithMessage("Receiver's account number does not exist");
-        RuleFor(e => e.ReceiverName).NotNull();
-        RuleFor(e => e.Description).NotNull();
+                var senderAccount = await dbContext.Accounts
+                    .FindAsync(new object?[] {args.SenderAccountId}, cancellationToken: cancellationToken);
+                return senderAccount?.Number != args.ReceiverAccountNumber;
+            })
+            .WithName("ReceiverAccountNumber")
+            .WithMessage("Cannot send money to yourself");
+
+        RuleFor(e => e.Amount)
+            .GreaterThan(0)
+            .WithMessage("Amount must be greater than 0");
+
+        RuleFor(e => e.ReceiverAccountNumber)
+            .MustAsync(async (numberAccount, cancellationToken) =>
+                {
+                    var result = await dbContext.Accounts
+                        .AnyAsync(number => number.Number == numberAccount, cancellationToken: cancellationToken);
+                    return result;
+                }
+            )
+            .WithMessage("Receiver's account number does not exist");
+
+        RuleFor(e => e.ReceiverName)
+            .NotEmpty()
+            .WithMessage("Receiver's name is required");
+
+        RuleFor(e => e.Description)
+            .NotEmpty()
+            .WithMessage("Description is required");
     }
 }
