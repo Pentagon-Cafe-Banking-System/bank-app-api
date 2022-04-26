a824badb874b3e035818d8e8a22cf82705f9fe3f
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-04-26 12:41:47 +0200

    refactor: remove unused register request dto

--- BankApp/Models/Requests/RegisterRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BankApp.Models.Requests;
-
-public class RegisterRequest
-{
-    public string UserName { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public string RoleName { get; set; } = string.Empty;
-}
